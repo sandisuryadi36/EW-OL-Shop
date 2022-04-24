c1264fb65932aa91999048439f9a4372d33ca94c
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-04-24 16:04:27 +0700

    fetch category

--- src/pages/admin/addProduct.jsx
+++ src/pages/admin/addProduct.jsx
@@ -1,6 +1,35 @@
 import Input from "../../component/Input";
+import { useState } from "react";
+import axios from "axios";
+import * as c from "../../app/data/constants"
+import "./index.scss";
 
 const AddProduct = () => { 
+    const [category, setCategory] = useState([]);
+
+    const loadCategory = async () => { 
+        let getCategory = await axios.get(c.API_URL + "/api/v1/category");
+        let categories = []
+        categories = getCategory.data.data.map(category => { 
+            return {"Id": category._id, "Name": category.name}
+        })
+        setCategory(categories);
+    }
+
+    const CategoryOptions = () => { 
+        console.log(category);
+        if (category.length < 1) { 
+            loadCategory();
+
+        }
+        console.log(category);
+        return (
+            category.map((cat, key) => {
+                return <option key={key} value={cat.Id}>{cat.Name}</option>
+            })
+        )
+    }
+
     return (
         <div>
             <h3>Add Product</h3>
@@ -9,11 +38,7 @@ const AddProduct = () => {
                 <Input name="description" type="text" placeholder="Deskripsi Produk..." label="Deskripsi" />
                 <Input name="category" type="select" placeholder="Kategori Produk..." label="Kategori">
                     <option value="">Pilih Kategori</option>
-                    <option>Shirts</option>
-                    <option>Jeans</option>
-                    <option>Jackets</option>
-                    <option>Sweaters</option>
-                    <option>Accessories</option>
+                    <CategoryOptions />
                 </Input>
                 <Input name="price" type="number" placeholder="Harga Produk..." label="Harga" />
                 <Input name="stock" type="number" placeholder="Stock Produk..." label="Stock" />
