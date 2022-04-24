04e372a52f15f26ff9fcd10c5fba2af3115887ae
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-04-24 14:31:04 +0700

    fix scss

--- src/component/Input/index.jsx
+++ src/component/Input/index.jsx
@@ -3,7 +3,7 @@ import './index.scss';
 const Input = (props) => {
   if(props.type === 'checkbox') {
     return (
-      <div>
+      <div className='checkbox'>
         <input className={props.error && 'is-invalid'} {...props} />
         <label>{props.label}</label>
         { props.error && props.error.map(err => <p key={err} className="invalid">* {err}</p>)}
