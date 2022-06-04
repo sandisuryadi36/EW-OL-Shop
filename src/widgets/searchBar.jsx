14e246f9347d3fb3216f17ce65408676fc9d162c
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-06-04 20:47:56 +0700

    fix seardhbar

--- src/widgets/searchBar.jsx
+++ src/widgets/searchBar.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = (props) => {
