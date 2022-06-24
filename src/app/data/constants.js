3eeb5ba4d6718e0c6c8d0ad766c902dc73899793
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-06-25 02:37:52 +0700

    .

--- src/app/data/constants.js
+++ src/app/data/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3001';
-// export const API_URL = 'https://ts-product-api.herokuapp.com';
+// export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://ts-product-api.herokuapp.com';
